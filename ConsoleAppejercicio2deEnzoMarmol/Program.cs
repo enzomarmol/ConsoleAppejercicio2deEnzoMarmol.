50dc530f1ea341b0c1278c07715883372316b246
enzo <enzo@LAPTOP-USB43B6I>
2022-07-05 21:57:02 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppejercicio2deEnzoMarmol/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleAppejercicio2deEnzoMarmol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado1, lado2, lado3, p, area, perimetro;
+
+            Console.WriteLine("ingrese el valor del lado1");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el valor del lado2");
+            lado2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el valor del lado3");
+            lado3 = double.Parse(Console.ReadLine());
+
+            p = (lado1 + lado2 + lado3) / 2;
+            Console.WriteLine($"el semiperimetro es {p}");
+
+
+
+            area = Math.Sqrt(p * (p - lado1) * (p - lado2) * (p - lado3));
+            Console.WriteLine($"el area es {area}");
+
+            perimetro = lado1 + lado2 + lado3;
+
+            Console.WriteLine($"el perimetro es {perimetro}");
+
+
+            if (Math.Pow(lado1, 2) == Math.Pow(lado2, 2) + Math.Pow(lado3, 2))
+            {
+                Console.Write($"es un triangulo rectangulo");
+            }
+            else
+            {
+                if (Math.Pow(lado1, 2) > Math.Pow(lado2, 2) + Math.Pow(lado3, 2))
+                {
+                    Console.Write($"es un triangulo obtusangulo");
+                }
+                else
+                {
+                    if (Math.Pow(lado1, 2) < Math.Pow(lado2, 2) + Math.Pow(lado3, 2))
+                    {
+                        Console.Write($"es un triangulo acutangulo");
+                    }
+                    else
+                    {
+                        Console.Write($"no es un triangulo");
+                        Console.ReadLine();
+                    }
+                }
+
+            }
+        }
+    }
+}
